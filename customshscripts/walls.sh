3d4105a42b3b87c129e6e499f6c45f86c7f6fce6
BeetleBot <94782272+BeetleBot@users.noreply.github.com>
2025-12-11 23:02:45 +0530

    Use HOME variable for wallpaper directory

--- customshscripts/walls.sh
+++ customshscripts/walls.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-WALLPAPER_DIR="/home/nkr/Pictures/Catpuccin-Wallpapers/"
+WALLPAPER_DIR="$HOME/Pictures/Catpuccin-Wallpapers/"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
