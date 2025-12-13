d1a15f5b2e2d088c7cc7faf98aef93c5e6bc9162
BeetleBot <nikarabilla@gmail.com>
2025-12-13 12:02:05 +0530

    Removed clipboard and powermenu

--- rofi/clipboard/clipboard.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-dir="$HOME/.config/rofi/launchers/"
-theme='style-3'
-
-## Run
-rofi \
-    rofi -modi clipboard:~/.config/rofi/clipboard/cliphist-rofi -show clipboard -theme ${dir}/${theme}.rasi    
