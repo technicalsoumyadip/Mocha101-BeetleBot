572d9482613edb87314724b2a4eccc40dccc305a
BeetleBot <nikarabilla@gmail.com>
2026-03-11 13:47:32 +0530

    New Rofi Config with no search button and minor changes in package_menu

--- rofi/package_menu.sh
+++ rofi/package_menu.sh
@@ -2,42 +2,141 @@
 
 terminal="kitty"
 
-choice=$(printf \
-"󰏖   Install package (PACMAN)\n\
-󰏖   Install package (AUR)\n\
-󰆴   Remove package\n\
-󰚰   Update system\n\
-󰄬   Clean orphan packages\n" \
-| rofi -dmenu -no-custom -disable-history -p "Package Manager")
+rofi_cmd="rofi -dmenu -theme ~/.config/rofi/NoSearchConfig.rasi"
+
+main_menu() {
+printf \
+"󰏖  INSTALL PACKAGES\n\
+󰆴  REMOVE PACKAGES\n\
+󰚰  UPDATE PACKAGES\n\
+󰄬  CLEAN ORPHAN PACKAGES\n\
+󰃢  CLEAN CACHE\n" | $rofi_cmd
+}
+
+install_menu() {
+printf \
+"󰏖  PACMAN\n\
+󰏖  AUR\n\
+  FLATPAK\n\
+󰜺  EXIT\n" | $rofi_cmd
+}
+
+remove_menu() {
+printf \
+"󰆴  PACMAN\n\
+󰆴  AUR\n\
+  FLATPAK\n\
+󰜺  EXIT\n" | $rofi_cmd
+}
+
+update_menu() {
+printf \
+"󰚰  PACMAN\n\
+󰚰  AUR\n\
+  FLATPAK\n\
+󰜺  EXIT\n" | $rofi_cmd
+}
+
+cache_menu() {
+printf \
+"󰃢  Clear Old Packages Cache\n\
+󰃢  Clear System Cache\n\
+󰜺  EXIT\n" | $rofi_cmd
+}
+
+choice=$(main_menu)
 
 case "$choice" in
 
-*"Install package (PACMAN)")
-    pkg=$(pacman -Slq | rofi -dmenu -i -p "Install (PACMAN)")
-    [ -n "$pkg" ] && $terminal -e sudo pacman -S "$pkg"
-    ;;
+*"INSTALL PACKAGES")
+    sub=$(install_menu)
 
-*"Install package (AUR)")
-    pkg=$(yay -Slq | rofi -dmenu -i -p "Install (AUR)")
-    [ -n "$pkg" ] && $terminal -e yay -S "$pkg"
-    ;;
+    case "$sub" in
 
-*"Remove package")
-    pkg=$(pacman -Qq | rofi -dmenu -i -p "Remove package")
-    [ -n "$pkg" ] && $terminal -e sudo pacman -R "$pkg"
-    ;;
+    *"PACMAN")
+        pkg=$(pacman -Slq | rofi -dmenu -i -p "Install (PACMAN)")
+        [ -n "$pkg" ] && $terminal -e bash -c "sudo pacman -S $pkg; echo; read -p 'Press Enter to close...'"
+        ;;
 
-*"Update system")
-    $terminal -e yay -Syu
-    ;;
+    *"AUR")
+        pkg=$(yay -Slq | rofi -dmenu -i -p "Install (AUR)")
+        [ -n "$pkg" ] && $terminal -e bash -c "yay -S $pkg; echo; read -p 'Press Enter to close...'"
+        ;;
 
-*"Clean orphan packages")
+    *"FLATPAK")
+        app=$(flatpak search --columns=application | rofi -dmenu -i -p "Install (FLATPAK)")
+        [ -n "$app" ] && $terminal -e bash -c "flatpak install flathub $app; echo; read -p 'Press Enter to close...'"
+        ;;
+
+    esac
+;;
+
+*"REMOVE PACKAGES")
+    sub=$(remove_menu)
+
+    case "$sub" in
+
+    *"PACMAN")
+        pkg=$(pacman -Qq | rofi -dmenu -i -p "Remove (PACMAN)")
+        [ -n "$pkg" ] && $terminal -e bash -c "sudo pacman -R $pkg; echo; read -p 'Press Enter to close...'"
+        ;;
+
+    *"AUR")
+        pkg=$(yay -Qqm | rofi -dmenu -i -p "Remove (AUR)")
+        [ -n "$pkg" ] && $terminal -e bash -c "yay -R $pkg; echo; read -p 'Press Enter to close...'"
+        ;;
+
+    *"FLATPAK")
+        app=$(flatpak list --app --columns=application | rofi -dmenu -i -p "Remove (FLATPAK)")
+        [ -n "$app" ] && $terminal -e bash -c "flatpak uninstall $app; echo; read -p 'Press Enter to close...'"
+        ;;
+
+    esac
+;;
+
+*"UPDATE PACKAGES")
+    sub=$(update_menu)
+
+    case "$sub" in
+
+    *"PACMAN")
+        $terminal -e bash -c "sudo pacman -Syu; echo; read -p 'Press Enter to close...'"
+        ;;
+
+    *"AUR")
+        $terminal -e bash -c "yay -Sua; echo; read -p 'Press Enter to close...'"
+        ;;
+
+    *"FLATPAK")
+        $terminal -e bash -c "flatpak update; echo; read -p 'Press Enter to close...'"
+        ;;
+
+    esac
+;;
+
+*"CLEAN ORPHAN PACKAGES")
     orphans=$(pacman -Qtdq)
     if [ -n "$orphans" ]; then
-        $terminal -e sudo pacman -Rns $orphans
+        $terminal -e bash -c "sudo pacman -Rns $orphans; echo; read -p 'Press Enter to close...'"
     else
         notify-send "Package Manager" "No orphan packages found"
     fi
-    ;;
+;;
+
+*"CLEAN CACHE")
+    sub=$(cache_menu)
+
+    case "$sub" in
+
+    *"Clear Old Packages Cache")
+        $terminal -e bash -c "sudo pacman -Sc; echo; read -p 'Press Enter to close...'"
+        ;;
+
+    *"Clear System Cache")
+        $terminal -e bash -c "sudo journalctl --vacuum-time=7d; sudo rm -rf /tmp/*; echo 'System cache cleaned'; echo; read -p 'Press Enter to close...'"
+        ;;
+
+    esac
+;;
 
 esac
