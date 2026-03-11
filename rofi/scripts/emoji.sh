b230926c7213bf3432238e284b9246c6b36588d3
BeetleBot <nikarabilla@gmail.com>
2026-03-11 15:30:02 +0530

    minor changes

--- rofi/scripts/emoji.sh
+++ rofi/scripts/emoji.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-
 rofimoji \
   --action type \
   --skin-tone ask \
-  --max-recent 5 \
+  --max-recent 0 \
   --prompt "Emoji" \
-  --selector-args="-theme-str '$THEME'"
+  --selector-args="-theme ~/.config/rofi/noleftpadding.rasi \
+  -theme-str 'element-icon { enabled: false; } element { spacing: 0px; }'"
+
