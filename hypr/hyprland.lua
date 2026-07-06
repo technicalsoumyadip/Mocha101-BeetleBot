9850276bdeac92515a7f929e47be50e7e7c8765a
BeetleBot <nikarabilla@gmail.com>
2026-07-06 23:53:50 +0530

    refactor: reduce border size in Hyprland configuration

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -80,7 +80,7 @@ hl.config({
     general = {
         gaps_in = 2,
         gaps_out = 5,
-        border_size = 1,
+        border_size = 0,
         col = {
             active_border = active_border,
             inactive_border = inactive_border,
